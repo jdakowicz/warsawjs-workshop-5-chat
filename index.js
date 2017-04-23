4c6e99aa59633b4d03682e945d990887c5c5af1b
Jakub <jakub.dakowicz@pagepro.pl>
2017-04-23 11:05:39 +0200

    basic server and package.json

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http')
+const { resolve } = require('path')
+const express = require('express')
+
+const PORT = process.env.PORT || '30001'
+const app = express()
+const server = http.Server(app)
+
+app.get('/', (req, res) => res.sendFile(resolve(__dirname, 'index.html')))
+server.listen(PORT, () => console.log(`listening on ${PORT}`))
