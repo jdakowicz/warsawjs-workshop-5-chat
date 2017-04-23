4543201a4157f4a8c09167748728f50dd9434687
Jakub <jakub.dakowicz@pagepro.pl>
2017-04-23 11:18:31 +0200

    new deps and initial files

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ const PORT = process.env.PORT || '30001'
 const app = express()
 const server = http.Server(app)
 
+require('./socketio')(server)
+
 app.get('/', (req, res) => res.sendFile(resolve(__dirname, 'index.html')))
 server.listen(PORT, () => console.log(`listening on ${PORT}`))
---
+++ socketio.js
@@ -0,0 +1,5 @@
+const socketio = require('socketio')
+
+module.exports = server => {
+  console.log('works')
+}
